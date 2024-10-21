8010d3a7fa51107efa06ed5b60b6d2d6c5a4ac00
mcmarius <23401453+mcmarius@users.noreply.github.com>
2024-10-21 14:29:15 +0300

    Modifică mesaj afișare

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <Helper.h>
 
 int main() {
-    std::cout << "Hello, world 2024!\n";
+    std::cout << "Hello, world, 2024-2025!\n";
     std::array<int, 100> v{};
     int nr;
     std::cout << "Introduceți nr: ";
